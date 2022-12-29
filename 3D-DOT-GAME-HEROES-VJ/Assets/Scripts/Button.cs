1db117615890659469d854cb8bf558af45ca763b
UriRius <80317497+UriRius@users.noreply.github.com>
2022-12-29 13:32:45 +0100

    puzles a mitjes

---
+++ 3D-DOT-GAME-HEROES-VJ/Assets/Scripts/Button.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private Material myMaterial;
+    private Color colorStart;
+    private Color nextColor;
+    private Color finalColor;
+    public bool unBlocked = false;
+    private int count = 0;
+
+    private void Start()
+    {
+        colorStart = GetComponent<Renderer>().material.color;
+        if (colorStart == Color.red){
+            nextColor = Color.green;
+            finalColor = Color.blue;
+        }
+        else if (colorStart == Color.green){
+            nextColor = Color.red;
+            finalColor = Color.blue;
+        }
+        else{
+            nextColor = Color.red;
+            finalColor = Color.green;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            switch(count) 
+            {
+            case 0:
+                GetComponent<Renderer>().material.color = nextColor;
+                break;
+            case 1:
+                GetComponent<Renderer>().material.color = finalColor;
+                break;
+            default:
+                GetComponent<Renderer>().material.color = colorStart;
+                count = -1;
+                break;
+            }
+            if (Color.green == GetComponent<Renderer>().material.color){
+                unBlocked = true;
+            }
+            else{
+                unBlocked = false;
+            }
+            Debug.Log("unblocked: " + unBlocked);
+            count ++;
+        }
+    }
+}
---
+++ 3D-DOT-GAME-HEROES-VJ/Assets/Scripts/CofrePuzzle3.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CofrePuzzle3 : MonoBehaviour
+{
+    public GameObject boto1;
+    public GameObject boto2;
+    public GameObject boto3;
+    public bool obert = false;
+
+    // private void Update()
+    // {
+    //     bool valorUnblocked1 = boto1.GetComponent<Button>().unblocked;
+    //     bool valorUnblocked2 = boto2.GetComponent<Button>().unblocked;
+    //     bool valorUnblocked3 = boto3.GetComponent<Button>().unblocked;
+
+    //     if (valorUnblocked1 && valorUnblocked2 && valorUnblocked3)
+    //     {
+    //         obert = true;
+    //     }
+
+    //     if( obert ){
+    //         Debug.Log("Cofre obert " + obert );
+    //     }
+    // }
+}
